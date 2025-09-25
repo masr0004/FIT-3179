54a46515d72cc3871794a3566b341b498d11c46e
masr0004 <marzana.asraf.03@gmail.com>
2025-09-26 02:24:21 +0800

    Updated HW9 visualization files to replace local paths with GitHub raw links (malaysia.vg.json, index.html, vega_lite_vis.js)

--- 34092544_HW9/js/vega_lite_vis.js
+++ 34092544_HW9/js/vega_lite_vis.js
@@ -1,6 +1,7 @@
 // This file loads the Vega-Lite spec
-var spec = "/js/malaysia.vg.json";
+var spec = "https://raw.githubusercontent.com/masr0004/FIT-3179/refs/heads/main/34092544_HW9/js/malaysia.vg.json";
 
 vegaEmbed('#vis', spec).then(function(result) {
   console.log("Chart rendered successfully");
 }).catch(console.error);
+
