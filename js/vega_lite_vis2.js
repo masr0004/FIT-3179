1ce025b7215ac4111ee6948043f830c8397944e8
masr0004 <marzana.asraf.03@gmail.com>
2025-09-26 02:55:17 +0800

    Delete js directory

--- js/vega_lite_vis2.js
+++
@@ -1,4 +0,0 @@
-var spec2 = "js/daily_cost_bar_chart.json";
-vegaEmbed('#bar_chart', spec2).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
