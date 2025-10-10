d3f2a3dbddac0beedbfa45f64da9fbdd66685072
masr0004 <marzana.asraf.03@gmail.com>
2025-10-11 01:13:47 +0800

    Add updated vega_lite_vis.js with GitHub raw links for charts

--- 34092544_HW_Week10/js/vega_lite_vis.js
+++ 34092544_HW_Week10/js/vega_lite_vis.js
@@ -1,29 +1,25 @@
-// Choropleth map 
-var mapSpec = "js/malaysia_state_map.json";
+/// Choropleth Map
+var mapSpec = "https://raw.githubusercontent.com/masr0004/FIT-3179/main/34092544_HW_Week10/js/malaysia_state_map.json";
 
 vegaEmbed('#choropleth_map', mapSpec).then(function(result) {
   console.log("Map chart rendered successfully");
   var panel = document.querySelector('#map-controls');
   if (panel) {
-    var container = result.view.container(); 
+    var container = result.view.container();
     var bindings = container && container.querySelector('.vega-bindings');
     if (bindings && !panel.contains(bindings)) panel.appendChild(bindings);
   }
 }).catch(console.error);
 
-//Dual Axis Plot
-var scatterSpec = "js/Dual_Axis_Bar_Line_Chart.json";
+// Dual Axis Plot
+var scatterSpec = "https://raw.githubusercontent.com/masr0004/FIT-3179/main/34092544_HW_Week10/js/Dual_Axis_Bar_Line_Chart.json";
 
 vegaEmbed('#dual_plot', scatterSpec).then(function(result) {
   console.log("Dual plot rendered successfully");
   var panel = document.querySelector('#dual-controls');
   if (panel) {
-    var container = result.view.container(); 
+    var container = result.view.container();
     var bindings = container && container.querySelector('.vega-bindings');
     if (bindings && !panel.contains(bindings)) panel.appendChild(bindings);
   }
 }).catch(console.error);
-
-
-
-
