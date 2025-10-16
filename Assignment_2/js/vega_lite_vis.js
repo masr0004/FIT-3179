29775b1f12c0e6fc9188242637cbbb0c1e1151e6
masr0004 <marzana.asraf.03@gmail.com>
2025-10-17 06:08:05 +0800

    Updated index.html and Vega embed logic script

--- Assignment_2/js/vega_lite_vis.js
+++ Assignment_2/js/vega_lite_vis.js
@@ -8,13 +8,12 @@ function embed(sel, specPath) {
     });
 }
 
-// IMPORTANT: paths are relative to index.html
-embed('#vis-waffle', 'js/waffle_chart.json');      // your waffle spec
-embed('#vis-lollipop', 'js/lollipop_chart.json');  // your lollipop spec
-embed('#vis-line', 'js/line_chart.json');          // your side-by-side GDP lines
-embed('#vis-line2',      'js/line_chart_2.json');
-embed('#vis-area', 'js/area_chart.json');          // monthly CPI-food index with year slider
-embed('#vis-grouped', 'js/grouped_bar_chart.json'); // grouped bar chart for employment by gender
-embed('#vis-map', 'js/map.json');
-embed('#vis-smallmultiples', 'js/small_multiples_graph.json');  
-embed('#vis-heatmap',    'js/heatmap.json');
+embed('#vis-waffle', 'https://raw.githubusercontent.com/masr0004/FIT-3179/refs/heads/main/Assignment_2/js/waffle_chart.json');
+embed('#vis-lollipop', 'https://raw.githubusercontent.com/masr0004/FIT-3179/refs/heads/main/Assignment_2/js/lollipop_chart.json');
+embed('#vis-line', 'https://raw.githubusercontent.com/masr0004/FIT-3179/refs/heads/main/Assignment_2/js/line_chart.json');
+embed('#vis-line2', 'https://raw.githubusercontent.com/masr0004/FIT-3179/refs/heads/main/Assignment_2/js/line_chart_2.json');
+embed('#vis-area', 'https://raw.githubusercontent.com/masr0004/FIT-3179/refs/heads/main/Assignment_2/js/area_chart.json');
+embed('#vis-grouped', 'https://raw.githubusercontent.com/masr0004/FIT-3179/refs/heads/main/Assignment_2/js/grouped_bar_chart.json');
+embed('#vis-map', 'https://raw.githubusercontent.com/masr0004/FIT-3179/refs/heads/main/Assignment_2/js/map.json');
+embed('#vis-smallmultiples', 'https://raw.githubusercontent.com/masr0004/FIT-3179/refs/heads/main/Assignment_2/js/small_multiples_graph.json');
+embed('#vis-heatmap', 'https://raw.githubusercontent.com/masr0004/FIT-3179/refs/heads/main/Assignment_2/js/heatmap.json');
